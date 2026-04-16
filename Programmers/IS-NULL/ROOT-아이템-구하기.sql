aab7c6600deddf45b194f078734e62c70407ca0a
moongs95 <juliaanswls@naver.com>
2026-04-16 20:58:06 +0900

    feat: ROOT 아이템 구하기 sql

---
+++ Programmers/IS-NULL/ROOT-아이템-구하기.sql
@@ -0,0 +1,7 @@
+-- ROOT 아이템을 찾아 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME)을 출력하는 SQL문을 작성해 주세요. 이때, 결과는 아이템 ID를 기준으로 오름차순 정렬해 주세요.
+select i.item_id
+    , i.item_name
+from item_info i
+    left join item_tree t on i.item_id = t.item_id
+where t.parent_item_id is null
+order by i.item_id asc
